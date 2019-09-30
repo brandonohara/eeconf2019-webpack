043b12a2d5874f27a45cf8df5eb69f48166322bf
Brandon O'Hara <brandon@brandonohara.com>
2019-09-30 13:57:50 -0600

    Add comment to show how including styles in javascript works.

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 /**
+ * If we want to include our SASS directly in Javascript,
+ * we can do it here
+ */
+ //require('./../styles/main.scss');
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['Authorization'] = `Bearer eeconf-2019-workshop`;
